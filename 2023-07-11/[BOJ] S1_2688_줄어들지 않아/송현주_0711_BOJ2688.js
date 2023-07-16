12b157412dc09d45f8141fbd2c6a95e7000a7cd6
HyunJu Song <38373150+shyunju7@users.noreply.github.com>
2023-07-16 16:03:11 +0900

    🐶 0711 송현주 문제풀이

---
+++ 2023-07-11/[BOJ] S1_2688_줄어들지 않아/송현주_0711_BOJ2688.js
@@ -0,0 +1,27 @@
+/* 
+[BOJ] 2688_줄어들지 않아_실버1
+https://www.acmicpc.net/problem/2688
+*/
+
+const solution = (input) => {
+  let answer = "";
+  const [T, ...numbers] = input.map((line) => parseInt(line));
+  const dp = Array.from({ length: 10 }, () => Array(65).fill(1));
+
+  for (let i = 1; i < 10; i++) {
+    dp[i][1] = 1;
+  }
+
+  for (let digit = 1; digit < 65; digit++) {
+    for (let num = 1; num < 10; num++) {
+      for (let k = 1; k <= digit; k++) {
+        dp[num][digit] += dp[num - 1][k];
+      }
+    }
+  }
+
+  for (let i = 0; i < T; i++) {
+    answer += dp[9][numbers[i]] + "\n";
+  }
+  return answer.trimEnd();
+};
