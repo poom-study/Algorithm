ed71aca53c5c3da576fc63b342020408a3a2b160
Sowon <63137536+sw0610@users.noreply.github.com>
2023-07-21 00:26:30 +0900

    🐣 0720 두소원 문제풀이

---
+++ 2023-07-20/[BOJ] G5_2293_동전 1/0720 두소원.py
@@ -0,0 +1,12 @@
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+coins = list(int(sys.stdin.readline()) for _ in range(n))
+
+dp = [0] * (k+1)
+dp[0] = 1
+for coin in coins:
+    for i in range(coin, k+1):
+        dp[i]+=dp[i-coin]
+
+print(dp[k])
