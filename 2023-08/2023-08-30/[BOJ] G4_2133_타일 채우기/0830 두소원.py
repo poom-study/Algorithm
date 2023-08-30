2c8cf14c44a540e7d1b5f8024cdd71234f9dbfbd
Sowon <63137536+sw0610@users.noreply.github.com>
2023-08-31 00:59:30 +0900

    🐣 0830 두소원 문제풀이

---
+++ 2023-08/2023-08-30/[BOJ] G4_2133_타일 채우기/0830 두소원.py
@@ -0,0 +1,14 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+dp = [0]*31
+
+dp[2] = 3
+
+for i in range(4, N+1, 2):
+    dp[i] = dp[i-2]*3+2
+    for j in range(2, i-2, 2):
+        dp[i]+=dp[j]*2
+
+print(dp[N])
